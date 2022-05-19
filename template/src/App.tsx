8f07552ce0980059ab74d5eb21416562954b872a
Guillaume Lacoste <guillaume.lacoste.e@thalesdigital.io>
2022-05-19 13:26:20 +0200

    update template link

--- template/src/App.tsx
+++ template/src/App.tsx
@@ -3,7 +3,10 @@ function App() {
     <div className="test">
       <h1>Tolkee&apos;s App</h1>
       <span>
-        App generated from this <a href="https://github.com/tolkee">template</a>
+        App generated from{' '}
+        <a href="https://github.com/tolkee/cra-template-tolkee">
+          cra-template-tolkee
+        </a>
       </span>
     </div>
   );
